92118fc323134c55de1975deb937933463809a02
Christopher Cardoso <cardoso.christopher01@gmail.com>
2022-05-23 16:53:34 -0400

    Enabling CORS

--- backend/stacks/ApiStack.js
+++ backend/stacks/ApiStack.js
@@ -18,6 +18,7 @@ export default class ApiStack extends sst.Stack {
         },
       },
       defaultAuthorizationType: "AWS_IAM",
+      cors: true,
       routes: {
         "POST   /notes": "src/create.main",
         "GET    /notes/{id}": "src/get.main",
